e2441a824ad7b4c52514f8ea5f5ca7c25f69308f
yonjon <yonjon@gmail.com>
2023-07-06 14:53:42 -0400

    Update streamlit_app.py
    Added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+streamlit.Header('Breakfast Header')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
