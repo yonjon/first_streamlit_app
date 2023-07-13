e8719ef9925ce33dfd40542fac8f9df4e4def3c0
yonjon <yonjon@gmail.com>
2023-07-13 10:14:48 -0400

    Update streamlit_app.py
    response data added

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruitvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
