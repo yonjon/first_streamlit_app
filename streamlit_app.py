afe22faa4cd0402e18029d561163bb48e41ad363
yonjon <yonjon@gmail.com>
2023-07-13 14:36:23 -0400

    update headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ except URLError as e:
     streamlit.error()
 
 # New section to display fruitvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('View Out Furit List - Add Your Favorites!')
 
 # snowflake related function
 def get_fruit_load_list():
@@ -49,9 +49,10 @@ def get_fruit_load_list():
       return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
@@ -63,5 +64,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add from snowf
 if streamlit.button('Add a Fruit to the List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
+   my_cnx.close()
    streamlit.text_input(back_from_function)
 
