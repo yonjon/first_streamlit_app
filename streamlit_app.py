414f5ab0bf870f28edfc9dae856af7d0794b53a2
yonjon <yonjon@gmail.com>
2023-07-13 13:31:56 -0400

    fetch all data

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
+# work on connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruid load list contrains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
