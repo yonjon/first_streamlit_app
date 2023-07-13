1a2f2b110bc03d49846c9e2d460c1fc2adf15184
yonjon <yonjon@gmail.com>
2023-07-13 10:11:51 -0400

    Update streamlit_app.py
    new cjha

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
@@ -19,5 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display fruitvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
 
